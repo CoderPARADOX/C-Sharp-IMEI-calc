5252097c57f835284b5a155d51076fd3be85d7a2
Mehmet Can ALTIN <mehmet.can.altin.mcan@gmail.com>
2021-03-28 14:46:44 +0300

    Imei Calc

---
+++ IMEI CALCULATOR/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace IMEI_CALCULATOR
+{
+    partial class ImeiCal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImeiCal));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LblTxtCount = new MetroSet_UI.Controls.MetroSetLabel();
+            this.metroSetLabel1 = new MetroSet_UI.Controls.MetroSetLabel();
+            this.LblWarn = new MetroSet_UI.Controls.MetroSetLabel();
+            this.TxtImeiOut = new MetroSet_UI.Controls.MetroSetTextBox();
+            this.TxtImei = new MetroSet_UI.Controls.MetroSetTextBox();
+            this.metroSetControlBox1 = new MetroSet_UI.Controls.MetroSetControlBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.LblTxtCount);
+            this.groupBox1.Controls.Add(this.metroSetLabel1);
+            this.groupBox1.Controls.Add(this.LblWarn);
+            this.groupBox1.Controls.Add(this.TxtImeiOut);
+            this.groupBox1.Controls.Add(this.TxtImei);
+            this.groupBox1.ForeColor = System.Drawing.Color.Aqua;
+            this.groupBox1.Location = new System.Drawing.Point(15, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 158);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ITEMS";
+            // 
+            // LblTxtCount
+            // 
+            this.LblTxtCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblTxtCount.IsDerivedStyle = true;
+            this.LblTxtCount.Location = new System.Drawing.Point(6, 31);
+            this.LblTxtCount.Name = "LblTxtCount";
+            this.LblTxtCount.Size = new System.Drawing.Size(251, 23);
+            this.LblTxtCount.Style = MetroSet_UI.Enums.Style.Dark;
+            this.LblTxtCount.StyleManager = null;
+            this.LblTxtCount.TabIndex = 7;
+            this.LblTxtCount.Text = ">";
+            this.LblTxtCount.ThemeAuthor = "Narwin";
+            this.LblTxtCount.ThemeName = "MetroDark";
+            // 
+            // metroSetLabel1
+            // 
+            this.metroSetLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.metroSetLabel1.IsDerivedStyle = true;
+            this.metroSetLabel1.Location = new System.Drawing.Point(263, 60);
+            this.metroSetLabel1.Name = "metroSetLabel1";
+            this.metroSetLabel1.Size = new System.Drawing.Size(22, 23);
+            this.metroSetLabel1.Style = MetroSet_UI.Enums.Style.Dark;
+            this.metroSetLabel1.StyleManager = null;
+            this.metroSetLabel1.TabIndex = 7;
+            this.metroSetLabel1.Text = ">";
+            this.metroSetLabel1.ThemeAuthor = "Narwin";
+            this.metroSetLabel1.ThemeName = "MetroDark";
+            // 
+            // LblWarn
+            // 
+            this.LblWarn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblWarn.IsDerivedStyle = true;
+            this.LblWarn.Location = new System.Drawing.Point(6, 102);
+            this.LblWarn.Name = "LblWarn";
+            this.LblWarn.Size = new System.Drawing.Size(251, 23);
+            this.LblWarn.Style = MetroSet_UI.Enums.Style.Dark;
+            this.LblWarn.StyleManager = null;
+            this.LblWarn.TabIndex = 6;
+            this.LblWarn.Text = "metroSetLabel1";
+            this.LblWarn.ThemeAuthor = "Narwin";
+            this.LblWarn.ThemeName = "MetroDark";
+            this.LblWarn.Visible = false;
+            // 
+            // TxtImeiOut
+            // 
+            this.TxtImeiOut.AutoCompleteCustomSource = null;
+            this.TxtImeiOut.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.TxtImeiOut.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.TxtImeiOut.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.TxtImeiOut.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.TxtImeiOut.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.TxtImeiOut.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.TxtImeiOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtImeiOut.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.TxtImeiOut.Image = null;
+            this.TxtImeiOut.IsDerivedStyle = true;
+            this.TxtImeiOut.Lines = null;
+            this.TxtImeiOut.Location = new System.Drawing.Point(291, 57);
+            this.TxtImeiOut.MaxLength = 3;
+            this.TxtImeiOut.Multiline = false;
+            this.TxtImeiOut.Name = "TxtImeiOut";
+            this.TxtImeiOut.ReadOnly = true;
+            this.TxtImeiOut.Size = new System.Drawing.Size(32, 28);
+            this.TxtImeiOut.Style = MetroSet_UI.Enums.Style.Light;
+            this.TxtImeiOut.StyleManager = null;
+            this.TxtImeiOut.TabIndex = 5;
+            this.TxtImeiOut.Text = "0";
+            this.TxtImeiOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TxtImeiOut.ThemeAuthor = "Narwin";
+            this.TxtImeiOut.ThemeName = "MetroLite";
+            this.TxtImeiOut.UseSystemPasswordChar = false;
+            this.TxtImeiOut.WatermarkText = "";
+            // 
+            // TxtImei
+            // 
+            this.TxtImei.AutoCompleteCustomSource = null;
+            this.TxtImei.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.TxtImei.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.TxtImei.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(110)))), ((int)(((byte)(110)))));
+            this.TxtImei.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.TxtImei.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtImei.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtImei.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtImei.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.TxtImei.Image = null;
+            this.TxtImei.IsDerivedStyle = true;
+            this.TxtImei.Lines = null;
+            this.TxtImei.Location = new System.Drawing.Point(6, 57);
+            this.TxtImei.MaxLength = 14;
+            this.TxtImei.Multiline = false;
+            this.TxtImei.Name = "TxtImei";
+            this.TxtImei.ReadOnly = false;
+            this.TxtImei.Size = new System.Drawing.Size(251, 30);
+            this.TxtImei.Style = MetroSet_UI.Enums.Style.Dark;
+            this.TxtImei.StyleManager = null;
+            this.TxtImei.TabIndex = 5;
+            this.TxtImei.Text = "00000000000000";
+            this.TxtImei.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.TxtImei.ThemeAuthor = "Narwin";
+            this.TxtImei.ThemeName = "MetroDark";
+            this.TxtImei.UseSystemPasswordChar = false;
+            this.TxtImei.WatermarkText = "";
+            this.TxtImei.TextChanged += new System.EventHandler(this.TxtImei_TextChanged);
+            this.TxtImei.Click += new System.EventHandler(this.TxtImei_Click);
+            this.TxtImei.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TxtImei_MouseDown);
+            // 
+            // metroSetControlBox1
+            // 
+            this.metroSetControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroSetControlBox1.CloseHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroSetControlBox1.CloseHoverForeColor = System.Drawing.Color.White;
+            this.metroSetControlBox1.CloseNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.DisabledForeColor = System.Drawing.Color.DimGray;
+            this.metroSetControlBox1.ForeColor = System.Drawing.Color.Aqua;
+            this.metroSetControlBox1.IsDerivedStyle = true;
+            this.metroSetControlBox1.Location = new System.Drawing.Point(306, -3);
+            this.metroSetControlBox1.MaximizeBox = false;
+            this.metroSetControlBox1.MaximizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroSetControlBox1.MaximizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MaximizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MinimizeBox = true;
+            this.metroSetControlBox1.MinimizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroSetControlBox1.MinimizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MinimizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.Name = "metroSetControlBox1";
+            this.metroSetControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.metroSetControlBox1.Style = MetroSet_UI.Enums.Style.Light;
+            this.metroSetControlBox1.StyleManager = null;
+            this.metroSetControlBox1.TabIndex = 4;
+            this.metroSetControlBox1.Text = "metroSetControlBox1";
+            this.metroSetControlBox1.ThemeAuthor = "Narwin";
+            this.metroSetControlBox1.ThemeName = "MetroLite";
+            // 
+            // ImeiCal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(402, 255);
+            this.Controls.Add(this.metroSetControlBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ImeiCal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Style = MetroSet_UI.Enums.Style.Dark;
+            this.Text = "IMEI CALCULATOR";
+            this.TextColor = System.Drawing.Color.White;
+            this.ThemeName = "MetroDark";
+            this.UseSlideAnimation = true;
+            this.Load += new System.EventHandler(this.ImeiCal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MetroSet_UI.Controls.MetroSetControlBox metroSetControlBox1;
+        private MetroSet_UI.Controls.MetroSetLabel metroSetLabel1;
+        private MetroSet_UI.Controls.MetroSetLabel LblWarn;
+        private MetroSet_UI.Controls.MetroSetTextBox TxtImeiOut;
+        private MetroSet_UI.Controls.MetroSetTextBox TxtImei;
+        private MetroSet_UI.Controls.MetroSetLabel LblTxtCount;
+    }
+}
+
---
+++ IMEI CALCULATOR/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMEI_CALCULATOR
+{
+    public partial class ImeiCal : MetroSet_UI.Forms.MetroSetForm
+    {
+        public ImeiCal()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(402, 255);//minsize
+            this.MaximumSize = new Size(402, 255);//max size
+        }
+
+        
+
+      
+     
+      
+
+        private void ImeiCal_Load(object sender, EventArgs e)
+        {
+        
+
+        }
+
+       
+       
+
+        private void TxtImei_Click(object sender, EventArgs e)
+        {
+           
+        }
+       
+    
+        static int sumDig(int n)//https://github.com/CoderPARADOX?tab=repositories
+        {
+            int a = 0;
+            while (n > 0)
+            {
+                a = a + n % 10;
+                n = n / 10;
+            }
+
+            return a;
+            //S. Thanks to Umut. H 
+        }
+
+        static int isValidIMEI(long n)
+        {
+
+            String s = n.ToString();
+            int len = s.Length;
+
+            int sum = 0;
+            for (int i = len; i >= 1; i--)
+            {
+                int d = (int)(n % 10);
+
+                if (i % 2 == 0)
+                    d = 2 * d;
+
+                sum += sumDig(d);
+                n = n / 10;
+            }
+            if (sum % 10 == 0)
+                return 0;
+            else
+                return (10 - (sum % 10));
+        }
+
+        private void TxtImei_TextChanged(object sender, EventArgs e)//https://github.com/CoderPARADOX?tab=repositories
+        {
+          LblTxtCount.Text ="IMEI Lenght : "+  TxtImei.Text.Length.ToString();
+            try
+            {
+
+                
+               
+                if (TxtImei.Text != "" &&TxtImei.Text.Length == 14)
+                {
+                    long CalcImei = Convert.ToInt64(TxtImei.Text);
+                    int LastNumb = isValidIMEI(CalcImei);
+                    TxtImeiOut.Text = LastNumb.ToString();
+                    LblWarn.Visible = false;
+                  
+                }
+                else
+                {
+                    LblWarn.Visible = true;
+                    LblWarn.Text = "Unable Character or empty!";
+                }
+
+            }
+            catch (Exception)
+            {
+                LblWarn.Visible = true;
+                LblWarn.Text = "Unable Character or empty!";
+                TxtImeiOut.Text = "0";
+               // MessageBox.Show("IMEI CANNOT BE EMPTY or letter", "[WARN]", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            if (TxtImei.Text == "")
+            {
+                TxtImeiOut.Text = "0";
+            }
+        }
+
+        private void TxtImei_MouseDown(object sender, MouseEventArgs e)
+        {
+            TxtImei.Text = string.Empty;
+        }
+    }
+}
